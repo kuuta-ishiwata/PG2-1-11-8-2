e141ec2f868011a4730a6592257a6640263f0b02
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2022-11-15 13:29:58 +0900

    player.cpp修正

--- player.h
+++ player.h
@@ -1,10 +1,22 @@
 #pragma once
+
+
 void WASDPush(float& theta, float& moveX, float& moveY, float& speed);
+
+void HitPlayerFanction
+(
+    float& CernterX, float& CernterY,
+    int radious,
+    float& LeToX, float& LeToY,
+    float& RiToX, float& RiToY,
+    float& LeBoX, float& LeBoY,
+    float& RiBoX, float& RiBoY
+);
 
 void BarrierPosfanction
 (
     float& BarrierTheta,
-    float& PlayerPosX, float& PlayerposY,
+    float& PlayerPosX, float& PlayerPosY,
     float& BarrierPosX, float& BarrierPosY
 );
 
@@ -16,3 +28,6 @@ void HitBoxFanction(
     float& LeBoX, float& LeBoY,
     float& RiBoX, float& RiBoY
 );
+
+
+
